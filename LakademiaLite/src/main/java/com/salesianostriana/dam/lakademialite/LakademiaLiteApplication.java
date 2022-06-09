06197925475b21c87232490d2510939f3c965556
jethrojonson <biojpg@gmail.com>
2022-06-10 01:00:26 +0200

    Se añade la página de index terminada y su controlador, se comienza el login y el error

--- LakademiaLite/src/main/java/com/salesianostriana/dam/lakademialite/LakademiaLiteApplication.java
+++ LakademiaLite/src/main/java/com/salesianostriana/dam/lakademialite/LakademiaLiteApplication.java
@@ -1,13 +1,7 @@
 package com.salesianostriana.dam.lakademialite;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.salesianostriana.dam.lakademialite.model.Alumno;
-import com.salesianostriana.dam.lakademialite.service.AlumnoService;
 
 @SpringBootApplication
 public class LakademiaLiteApplication {
@@ -15,31 +9,4 @@ public class LakademiaLiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LakademiaLiteApplication.class, args);
 	}
-	
-	@Bean
-	public CommandLineRunner init(AlumnoService servicio, BCryptPasswordEncoder passwordEncoder) {
-		return args -> {
-			
-			Alumno a = new Alumno();
-			a.setAdmin(false);
-			a.setNombreAlumno("Jerónimo M.");
-			a.setApellidosAlumno("Pérez González");
-			a.setEmail("jero_perez@email.com");
-			a.setPassword(passwordEncoder.encode("1234"));
-			
-			servicio.save(a);
-			
-			
-			Alumno b = new Alumno();
-			b.setAdmin(true);
-			b.setNombreAlumno("Luismi");
-			a.setApellidosAlumno("Lopez");
-			a.setEmail("luismi.lopez@email.com");
-			a.setPassword(passwordEncoder.encode("1234"));
-			
-			servicio.save(b);
-			
-		};
-	}
-
 }
